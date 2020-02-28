4d5a9e59d8bbb8244415d6de8b4b6db5d2fdd414
Mark A. Garrett <magarret@go.olemiss.edu>
2020-02-28 00:31:57 -0600

    add direction markers, add tracer moons, try adding tilt to the orbit.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -18,33 +18,27 @@ public class CameraController : MonoBehaviour
         //Translation controls
         if (Input.GetKey(KeyCode.W))
         {
-            Debug.Log("Forward");
             Translation.z += TranslationStep;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            Debug.Log("Backward");
             Translation.z -= TranslationStep;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            Debug.Log("Left");
             Translation.x -= TranslationStep;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            Debug.Log("Right");
             Translation.x += TranslationStep;
         }
 
         if (Input.GetKey(KeyCode.Q))
         {
-            Debug.Log("Up");
             Translation.y += TranslationStep;
         }
         if (Input.GetKey(KeyCode.E))
         {
-            Debug.Log("Down");
             Translation.y -= TranslationStep;
         }
 
@@ -52,36 +46,30 @@ public class CameraController : MonoBehaviour
         //Pitch
         if (Input.GetKey(KeyCode.Home))
         {
-            Debug.Log("Pitch Up");
             Rotation.x -= RotationStep;
         }
         if (Input.GetKey(KeyCode.End))
         {
-            Debug.Log("Pitch Down");
             Rotation.x += RotationStep;
         }
 
         //Bank
         if (Input.GetKey(KeyCode.PageUp))
         {
-            Debug.Log("Bank Left");
             Rotation.z -= RotationStep;
         }
         if (Input.GetKey(KeyCode.Insert))
         {
-            Debug.Log("Bank Right");
             Rotation.z += RotationStep;
         }
 
         //Yaw
         if (Input.GetKey(KeyCode.Delete))
         {
-            Debug.Log("Yaw Left");
             Rotation.y -= RotationStep;
         }
         if (Input.GetKey(KeyCode.PageDown))
         {
-            Debug.Log("Yaw Right");
             Rotation.y += RotationStep;
         }
 
--- Assets/Scripts/OrbitManager.cs
+++ Assets/Scripts/OrbitManager.cs
@@ -6,11 +6,17 @@ public class OrbitManager : MonoBehaviour
 {
     public float tacometer = 0f;
     public float speed = 100f;
+    private float RealSpeed;
     public Transform parentBody;
     public Transform childBody;
+    public Transform nextBody;
     public float radius;
-
+    public float CoupleLenght = 75;
+    private float volocity;
     private float theta;
+    private float x;
+    private float z;
+
     //arch lenght formula D = 2 * PI * radius * theta
     //rearranged theta = D / (2 * PI * radius)
 
@@ -18,19 +24,46 @@ public class OrbitManager : MonoBehaviour
     void Start()
     {
         radius = Vector3.Distance(parentBody.position, childBody.position);
+        volocity = speed;
         //Debug.Log(radius);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            volocity = speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            volocity = - speed;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            volocity = 0;
+        }
+
         if (Input.GetKey(KeyCode.Return)){
             Debug.Log(radius * Mathf.Sin(theta) + ":" + radius * Mathf.Tan(theta));
         }
-        tacometer += speed * Time.deltaTime;
+        if(nextBody != null && CoupleLenght > Vector3.Distance(childBody.position, nextBody.position))
+        {
+            return;
+        }
+        tacometer += volocity * Time.deltaTime;
         theta = tacometer / (2 * Mathf.PI * radius);
+        x = radius * Mathf.Cos(theta);
+        z = radius * Mathf.Sin(theta);
         //Debug.Log(radius * (1 / Mathf.Sin(theta)) + ":" + radius * (1 / Mathf.Tan(theta)));
-        childBody.position = new Vector3 (radius * Mathf.Cos(theta), 0, radius * Mathf.Sin(theta));
-
+        //childBody.position = new Vector3 (radius * Mathf.Cos(theta), 0, radius * Mathf.Sin(theta));
+        //childBody.position = new Vector3(x, (z * .5f) + (x * .5f) , z);  //Apex NE
+        //childBody.position = new Vector3(x, (z * .5f) + (x * - .5f), z); //Apex NW
+        //childBody.position = new Vector3(x, (z * - .5f) + (x * .5f), z); //Apex SE
+        //childBody.position = new Vector3(x, (z * -.5f) + (x * -.5f), z);   //Apex SW
+        //childBody.position = new Vector3(x, (x), z); //Apex E
+        childBody.position = new Vector3(x, (z), z);   // Apex N
+        //childBody.position = new Vector3(x, (-x), z); //Apex W
+        //childBody.position = new Vector3(x, (-z), z); //Apex S
     }
 }
