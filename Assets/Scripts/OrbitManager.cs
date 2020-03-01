19a27b0505e29136ba9fa945a92687dd1d2b1837
Mark A. Garrett <magarret@go.olemiss.edu>
2020-02-29 18:09:14 -0600

    orbit the parent body not the origin, multiple volocity by Time.deltaTime

--- Assets/Scripts/OrbitManager.cs
+++ Assets/Scripts/OrbitManager.cs
@@ -42,27 +42,23 @@ public class OrbitManager : MonoBehaviour
         {
             volocity = 0;
         }
-
-        if (Input.GetKey(KeyCode.Return)){
-            Debug.Log(radius * Mathf.Sin(theta) + ":" + radius * Mathf.Tan(theta));
-        }
         if(nextBody != null && CoupleLenght > Vector3.Distance(childBody.position, nextBody.position))
         {
             return;
         }
-        theta = (theta + volocity / (2 * Mathf.PI * radius)) % 360;
+        theta = (theta + (volocity * Time.deltaTime) / (2 * Mathf.PI * radius)) % 360;
         Debug.Log(volocity);
         x = radius * Mathf.Cos(theta);
         z = radius * Mathf.Sin(theta);
         //Debug.Log(radius * (1 / Mathf.Sin(theta)) + ":" + radius * (1 / Mathf.Tan(theta)));
-        childBody.position = new Vector3 (radius * Mathf.Cos(theta), 0, radius * Mathf.Sin(theta));
-        //childBody.position = new Vector3(x, (z * .5f) + (x * .5f) , z);  //Apex NE
-        //childBody.position = new Vector3(x, (z * .5f) + (x * - .5f), z); //Apex NW
-        //childBody.position = new Vector3(x, (z * - .5f) + (x * .5f), z); //Apex SE
-        //childBody.position = new Vector3(x, (z * -.5f) + (x * -.5f), z);   //Apex SW
-        //childBody.position = new Vector3(x, (x), z); //Apex E
-        //childBody.position = new Vector3(x, (z), z);   // Apex N
-        //childBody.position = new Vector3(x, (-x), z); //Apex W
-        //childBody.position = new Vector3(x, (-z), z); //Apex S
+        childBody.position = parentBody.position + new Vector3 (radius * Mathf.Cos(theta), 0, radius * Mathf.Sin(theta));
+        //childBody.position = parentBody.position + new Vector3(x, (z * .5f) + (x * .5f) , z);  //Apex NE
+        //childBody.position = parentBody.position + new Vector3(x, (z * .5f) + (x * - .5f), z); //Apex NW
+        //childBody.position = parentBody.position + new Vector3(x, (z * - .5f) + (x * .5f), z); //Apex SE
+        //childBody.position = parentBody.position + new Vector3(x, (z * -.5f) + (x * -.5f), z);   //Apex SW
+        //childBody.position = parentBody.position + new Vector3(x, (x), z); //Apex E
+        //childBody.position = parentBody.position + new Vector3(x, (z), z);   // Apex N
+        //childBody.position = parentBody.position + new Vector3(x, (-x), z); //Apex W
+        //childBody.position = parentBody.position + new Vector3(x, (-z), z); //Apex S
     }
 }
